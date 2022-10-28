687048f1d9472b64ecb57818b6ebd0fea401ccbd
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-10-28 18:23:46 +0530

    Migrations

---
+++ 20221027104745_Collab.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class Collab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    ModifiedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Note = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    IsArchive = table.Column<bool>(nullable: false),
+                    IsPin = table.Column<bool>(nullable: false),
+                    IsTrash = table.Column<bool>(nullable: false),
+                    userid = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteID);
+                    table.ForeignKey(
+                        name: "FK_Notes_Users_userid",
+                        column: x => x.userid,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollaboratorTable",
+                columns: table => new
+                {
+                    CollaboratorID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollaboratedEmail = table.Column<string>(nullable: true),
+                    NoteID = table.Column<long>(nullable: false),
+                    userid = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollaboratorTable", x => x.CollaboratorID);
+                    table.ForeignKey(
+                        name: "FK_CollaboratorTable_Notes_NoteID",
+                        column: x => x.NoteID,
+                        principalTable: "Notes",
+                        principalColumn: "NoteID",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_CollaboratorTable_Users_userid",
+                        column: x => x.userid,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollaboratorTable_NoteID",
+                table: "CollaboratorTable",
+                column: "NoteID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollaboratorTable_userid",
+                table: "CollaboratorTable",
+                column: "userid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_userid",
+                table: "Notes",
+                column: "userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollaboratorTable");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ 20221027131939_Label.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class Label : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lableTable",
+                columns: table => new
+                {
+                    LabelID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LabelName = table.Column<string>(nullable: true),
+                    noteID = table.Column<long>(nullable: false),
+                    UserId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lableTable", x => x.LabelID);
+                    table.ForeignKey(
+                        name: "FK_lableTable_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_lableTable_Notes_noteID",
+                        column: x => x.noteID,
+                        principalTable: "Notes",
+                        principalColumn: "NoteID",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_lableTable_UserId",
+                table: "lableTable",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_lableTable_noteID",
+                table: "lableTable",
+                column: "noteID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lableTable");
+        }
+    }
+}
