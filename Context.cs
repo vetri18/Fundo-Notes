c441b4e25095dfb38e0d6edee34bf52141a78d93
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-10-28 18:20:48 +0530

    1-Context

---
+++ Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.AddContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<NotesEntity> Notes { get; set; }
+        public DbSet<CollaboratorEntity> CollaboratorTable { get; set; }
+        public DbSet<LabelEntity> lableTable { get; set; }
+
+    }
+}
+
+
